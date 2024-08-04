1473025cfbfc2ece9833201e8bcbb6db8c2e7e33
rpa0309 <anstettm@ufl.edu>
2024-08-04 14:43:00 -0400

    Fix CMake Files to work for multiple OS

--- cmake-build-release/cmake_install.cmake
+++ cmake-build-release/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Jess/OneDrive/Documents/GitHub/KeyStroke Crafters
+# Install script for directory: /Users/mirandaanstett/Documents/GitHub/KeyStroke-Crafters
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/KeyStroke_Crafters")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/mingw6/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jess/OneDrive/Documents/GitHub/KeyStroke Crafters/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/mirandaanstett/Documents/GitHub/KeyStroke-Crafters/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
